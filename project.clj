8108b825ffa69929e1c691fecbc97e829de6c8f5
Alexander Scott <xander@axrs.io>
2019-11-21 09:06:44 +1000

    Update Deps

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def slf4j-version "1.7.26")
 
-(defproject io.jesi/parent "1.0.1"
+(defproject io.jesi/parent "1.0.2"
   :description "Clojure and NPM parent package definitions"
   :url "https://github.com/jesims/parent#readme"
   :license {:name         "Eclipse Public License - v 1.0"
@@ -9,15 +9,15 @@
             :comments     "same as Clojure"}
   :min-lein-version "2.8.1"
   :managed-dependencies [[org.clojure/clojure "1.10.1"]
-                         [thheller/shadow-cljs "2.8.41"]
-                         [io.jesi/backpack "3.5.0"]
-                         [com.rpl/specter "1.1.2"]
-                         [org.clojure/core.async "0.4.490"]
+                         [thheller/shadow-cljs "2.8.74"]
+                         [io.jesi/backpack "3.7.0"]
+                         [com.rpl/specter "1.1.3"]
+                         [org.clojure/core.async "0.5.527"]
                          ;DB
-                         [org.clojure/java.jdbc "0.7.9"]
-                         [org.postgresql/postgresql "42.2.1"]
+                         [org.clojure/java.jdbc "0.7.10"]
+                         [org.postgresql/postgresql "42.2.8"]
                          [clj-postgresql "0.7.0"]
-                         [nilenso/honeysql-postgres "0.2.4"]
+                         [nilenso/honeysql-postgres "0.2.6"]
                          ;Logging
                          [ch.qos.logback/logback-classic "1.2.3"]
                          [org.slf4j/jcl-over-slf4j ~slf4j-version]
