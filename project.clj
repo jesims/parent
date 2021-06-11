27ed6dfb3c5dc20d666856a85e83f78831b21323
André de Jager <andrethehunter@gmail.com>
2021-06-11 10:25:13 +1000

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.jesi/parent "4.14.0"
+(defproject io.jesi/parent "4.15.0"
   :description "Clojure and NPM parent package definitions"
   :url "https://github.com/jesims/parent#readme"
   :license {:name         "Eclipse Public License - v 1.0"
