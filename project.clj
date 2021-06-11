77ac9eb0451f9f28e585d14b2fe247866d49bdba
André de Jager <andrethehunter@gmail.com>
2021-06-11 10:23:47 +1000

    Update backpack

--- project.clj
+++ project.clj
@@ -8,7 +8,7 @@
   :min-lein-version "2.9.1"
   :managed-dependencies [[org.clojure/clojure "1.10.3"]
                          [thheller/shadow-cljs "2.14.2"]
-                         [io.jesi/backpack "7.1.0"]
+                         [io.jesi/backpack "7.2.0"]
                          [io.jesi/customs "1.3.2"]
                          [com.rpl/specter "1.1.3"]
                          [org.clojure/core.async "1.3.618"]
